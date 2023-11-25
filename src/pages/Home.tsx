a6e61e51f309d0d4dbeb95430ec4d294e670e6a1
Fakhteh <software@panto.org>
2023-11-25 05:17:10 +0330

    feat: 🎸 implement home page section
    responsive main page for different devices

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,16 +8,16 @@ export const Home = ({ searchCard }) => {
   );
   return (
     <div className="flex justify-between">
-      <div className="grid grid-cols-4 my-10 font-regular ml-20">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-10 font-regular ml-20">
         {result.map((item) => (
           <div key={item.id} className="ml-10 justify-center mb-10 ">
-            <div className="w-64 h-14 bg-premitiveWhite shadow rounded-t-[15px] items-center py-3.5 px-6 text-bw1 text-sm font-normal leading-7">
+            <div className="w-auto h-auto bg-[#F8F8F8] shadow rounded-t-[15px] items-center py-3.5 px-6 text-bw1 text-sm font-normal leading-7">
               {item.name}
             </div>
             <Link to={`/paint/${item.id}`}>
-              <img className="w-64 h-96" src={item.image} />
+              <img className="w-64 h-96 duration-700 transition hover:scale-125" src={item.image} />
             </Link>
-            <div className="w-64 h-20 bg-premitiveWhite shadow rounded-b-[15px] justify-start items-center py-3.5 px-6 text-bw1 font-normal  text-xs">
+            <div className="w-64 h-20 bg-[#F8F8F8] shadow rounded-b-[15px] justify-start items-center py-3.5 px-6 text-bw1 font-normal  text-xs">
               <div className="flex items-center">
                 <img src={Vector} className="w-6 h-6 mr-2 pb-1 " />
                 {item.artist}
